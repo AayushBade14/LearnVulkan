7782c0f7f8246319bde2b67efb6ab0a4b2a5365d
AayushBade14 <aayushrajubade@gmail.com>
2026-01-01 17:02:31 +0530

    Modified main.cpp : Added validation layers

--- main.cpp
+++ main.cpp
@@ -4,9 +4,21 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <cstring>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+  "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+  const bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication
 {
@@ -36,8 +48,43 @@ private:
     window = glfwCreateWindow(800, 600, "Vulkan", nullptr, nullptr);
   }
   
+  bool checkValidationLayerSupport()
+  {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for(const char* layerName : validationLayers)
+    {
+      bool layerFound=  false;
+
+      for(const auto& layerProperties : availableLayers)
+      {
+        if(strcmp(layerName, layerProperties.layerName) == 0)
+        {
+          layerFound = true;
+          break;
+        }
+      }
+
+      if(!layerFound)
+      {
+        return false;
+      }
+    }
+
+    return true;
+  }
+  
   void createInstance()
   {
+    if(enableValidationLayers && !checkValidationLayerSupport())
+    {
+      throw std::runtime_error("validation layers requested, but not available!");
+    }
+
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello Triangle";
@@ -55,7 +102,16 @@ private:
     createInfo.pApplicationInfo = &appInfo;
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
-    createInfo.enabledLayerCount = 0;
+    
+    if(enableValidationLayers)
+    {
+      createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+      createInfo.ppEnabledLayerNames = validationLayers.data();
+    }
+    else
+    {
+      createInfo.enabledLayerCount = 0;
+    }
 
     if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
     {
@@ -78,7 +134,9 @@ private:
   }
 
   void cleanup()
-  {
+  { 
+    vkDestroyInstance(instance, nullptr);
+    
     glfwDestroyWindow(window);
 
     glfwTerminate();
